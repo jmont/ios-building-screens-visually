f4c58becc3d486c83ace604e1f94f36001f07c0c
Juan C. Montemayor E <juan.montemayor@nytimes.com>
2018-11-26 14:04:15 -0500

    initial commit

---
+++ BuildingScreensVisually/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildingScreensVisually
+//
+//  Created by Montemayor Elosua, Juan Carlos on 11/26/18.
+//  Copyright © 2018 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
